b1074028f9d77b290f3d97bf41b1771cd1157592
Marco Vanzo <marco@mv-consulting.it>
2026-07-03 22:17:11 +0200

    fix: redirect alla lista atleti dopo salvataggio roster
    Aggiunto getRedirectUrl() in EditRoster e EditYouthRoster per tornare
    alla pagina con tutte le atlete dopo aver cliccato Salva.

--- app/Filament/Resources/YouthRosterResource/Pages/EditYouthRoster.php
+++ app/Filament/Resources/YouthRosterResource/Pages/EditYouthRoster.php
@@ -10,6 +10,11 @@ class EditYouthRoster extends EditRecord
 {
     protected static string $resource = YouthRosterResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
