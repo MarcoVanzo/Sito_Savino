d28da52636aa733b88579e937a0403a6f60e309e
Marco Vanzo <marco@mv-consulting.it>
2026-07-11 16:24:18 +0200

    debug: add temporary database inspection and seeding routes

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,20 @@ Route::domain('shop.savinodelbenevolley.it')->group(function () {
     })->where('any', '.*');
 });
 
+Route::get('/debug-db', function() {
+    return [
+        'pages' => \App\Models\Page::select('id', 'slug', 'status', 'title')->get()
+    ];
+});
+
+Route::get('/debug-db-seed', function() {
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--class' => 'CmsPagesSeeder']);
+    return [
+        'output' => \Illuminate\Support\Facades\Artisan::output(),
+        'pages' => \App\Models\Page::select('id', 'slug', 'status', 'title')->get()
+    ];
+});
+
 $locales = ['it', 'en'];
 
 foreach ($locales as $loc) {
