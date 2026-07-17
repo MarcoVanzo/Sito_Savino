6557aa68a32f609e82cf86e5971cc676e1db1fe1
MarcoVanzo <marco@mv-consulting.it>
2026-07-17 22:19:03 +0200

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\ForceChangePasswordController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
@@ -58,10 +59,10 @@ Route::middleware('auth')->group(function () {
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::get('change-password', [\App\Http\Controllers\Auth\ForceChangePasswordController::class, 'show'])
+    Route::get('change-password', [ForceChangePasswordController::class, 'show'])
         ->name('password.change');
 
-    Route::post('change-password', [\App\Http\Controllers\Auth\ForceChangePasswordController::class, 'store'])
+    Route::post('change-password', [ForceChangePasswordController::class, 'store'])
         ->name('password.change.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
