3ace6e2c0a7fd32c9cfb6a41bc85caea9904fe16
MarcoVanzo <marco@mv-consulting.it>
2026-07-17 22:26:30 +0200

    Update EnsurePasswordIsChangedTest.php

--- tests/Feature/EnsurePasswordIsChangedTest.php
+++ tests/Feature/EnsurePasswordIsChangedTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
 use App\Enums\UserRole;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
