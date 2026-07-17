427e1117b67e6b3d40650bdafb6b0c07a59909f6
MarcoVanzo <marco@mv-consulting.it>
2026-07-17 23:57:06 +0200

    style: fix phpdoc_indent — remove extra space before closing */

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ use Illuminate\Notifications\Notifiable;
 
 /**
  * @property UserRole $role
-  */
+ */
 #[Fillable(['name', 'email', 'password', 'phone', 'address', 'is_active', 'role', 'must_change_password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements FilamentUser, MustVerifyEmail
